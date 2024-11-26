593b8e9f69eb5a28c19fe1186ba48e3f80fadfeb
Alex Grover <hello@alexgrover.me>
2024-11-26 00:17:33 -0500

    Set up ConnectKit, remove default files

---
+++ src/lib/Web3Provider.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import { WagmiProvider, createConfig, http } from "wagmi";
+import { base } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+import { PropsWithChildren } from "react";
+
+const config = createConfig(
+  getDefaultConfig({
+    // Your dApps chains
+    chains: [base],
+    transports: {
+      // RPC URL for each chain
+      [base.id]: http(
+        `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`,
+      ),
+    },
+
+    // Required API Keys
+    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+
+    // Required App Info
+    appName: "RGB Signatures",
+
+    // Optional App Info
+    appDescription: "RGB is an infinite canvas",
+    appUrl: "https://rgb.so", // your app's url
+    appIcon: "", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+  }),
+);
+
+const queryClient = new QueryClient();
+
+export const Web3Provider = ({ children }: PropsWithChildren) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectKitProvider>{children}</ConnectKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
