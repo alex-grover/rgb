8fa8388c0f6fd099012d3827c7d17f8bc6c25744
Alex Grover <hello@alexgrover.me>
2024-12-12 14:02:34 -0500

    Update allowlist input file to csv

--- src/scripts/allowlist.ts
+++ src/scripts/allowlist.ts
@@ -1,7 +1,11 @@
 import { StandardMerkleTree } from '@openzeppelin/merkle-tree'
 
-const file = Bun.file('./src/scripts/allowlist.json') // path is relative to cwd, which is project root when run as package.json script
-const values = (await file.json()) as string[][]
+const file = Bun.file('./src/scripts/allowlist.csv') // path is relative to cwd, which is project root when run as package.json script
+const text = await file.text()
+const values = text
+  .split('\n')
+  .filter((value) => !!value)
+  .map((value) => [value])
 
 const tree = StandardMerkleTree.of(values, ['address'])
 
