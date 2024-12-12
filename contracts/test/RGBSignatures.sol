fd34ab97b2992318082116b02b034c8dc600a19d
Alex Grover <hello@alexgrover.me>
2024-12-11 20:40:30 -0500

    Allow setting owner

--- contracts/test/RGBSignatures.sol
+++ contracts/test/RGBSignatures.sol
@@ -5,9 +5,11 @@ import {Test, console} from "forge-std/Test.sol";
 import {RGBSignatures} from "../src/RGBSignatures.sol";
 
 contract RGBSignaturesTest is Test {
-    function test_tokenURI(address payable feeRecipient) external {
-        vm.assume(feeRecipient != address(0));
-        RGBSignatures signatures = new RGBSignatures(0.004 ether, 0.001 ether, feeRecipient);
+    function test_tokenURI(address owner, uint256 mintCost, uint256 randomMintCost, address payable feeRecipient)
+        external
+    {
+        vm.assume(owner != address(0) && feeRecipient != address(0));
+        RGBSignatures signatures = new RGBSignatures(owner, mintCost, randomMintCost, feeRecipient);
         console.log(signatures.tokenURI(signatures.tokenId(255, 153, 0)));
     }
 }
