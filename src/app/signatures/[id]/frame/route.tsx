405fd9410f4deb81caac4452ddb45d7cd4414092
Alex Grover <hello@alexgrover.me>
2024-12-14 16:01:37 -0500

    Revert "Remove frame"
    This reverts commit 8b4596e13b9a8b9b1b9196ff617316b2a7e6d175.

---
+++ src/app/signatures/[id]/frame/route.tsx
@@ -0,0 +1,31 @@
+/** @jsxImportSource frog/jsx */
+
+import { Button, Frog } from 'frog'
+import { handle } from 'frog/next'
+
+const app = new Frog({
+  title: 'RGB',
+  imageAspectRatio: '1:1',
+  imageOptions: {
+    height: 1000,
+    width: 1000,
+  },
+})
+
+app.frame('/signatures/:id/frame', (c) => {
+  const url = process.env.VERCEL_URL ?? 'http://localhost:3000'
+
+  return c.res({
+    image: `/signatures/${c.req.param('id')}/frame/image`,
+    intents: [
+      // biome-ignore lint/correctness/useJsxKeyInIterable: n/a
+      <Button.Link href={`${url}/signatures/${c.req.param('id')}`}>
+        View
+      </Button.Link>,
+      // biome-ignore lint/correctness/useJsxKeyInIterable: n/a
+      <Button.Link href={url}>Mint</Button.Link>,
+    ],
+  })
+})
+
+export const GET = handle(app)
