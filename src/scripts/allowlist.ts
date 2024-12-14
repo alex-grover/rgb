9d48a3cdc781e49088142bf8739bbe7d0e12e162
Alex Grover <hello@alexgrover.me>
2024-12-14 16:11:43 -0500

    Only add addresses to merkle tree

--- src/scripts/allowlist.ts
+++ src/scripts/allowlist.ts
@@ -1,10 +1,11 @@
 import { StandardMerkleTree } from '@openzeppelin/merkle-tree'
+import { isAddress } from 'viem'
 
 const file = Bun.file('./src/scripts/allowlist.csv') // path is relative to cwd, which is project root when run as package.json script
 const text = await file.text()
 const values = text
   .split('\n')
-  .filter((value) => !!value)
+  .filter((value) => isAddress(value))
   .map((value) => [value])
 
 const tree = StandardMerkleTree.of(values, ['address'])
