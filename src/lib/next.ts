fd1836465495c16c551a83be445fb3ebed33dba2
Alex Grover <hello@alexgrover.me>
2024-12-16 21:37:20 -0500

    Fix env var

--- src/lib/next.ts
+++ src/lib/next.ts
@@ -8,7 +8,7 @@ export type PageProps = RouteContext & {
 
 export function getUrl() {
   if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production')
-    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
   if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview')
     return `https://${process.env.VERCEL_URL}`
   return 'http://localhost:3000'
